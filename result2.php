feabd71dbead151db48bdce3007740caedea41bf
李继庠 <nusseres@gmail.com>
2016-04-09 17:30:21 +0800

    resource folder will be initialized everytime upload input images

---
+++ result2.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Off-canvas navigation menu Tutorial">
+    <meta name="keywords" content="slide-menu, sidebar, off-canvas, menu, navigation" />
+    <meta name="author" content="AcasA Programming">
+
+    <title>Off-canvas navigation menu Tutorial</title>
+
+    <link rel="stylesheet" type="text/css" href="css/app.css" />
+    <link rel="stylesheet" type="text/css" href="css/normalize.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/slide.css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/fileinput.css" />
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  </head>
+
+  <body>
+
+  <div class="container">
+    This is some awesome text.
+
+    <div class="well">
+    <div class="row">
+        <div class="col-lg-12">
+            <h3>Origin</h3>
+            <img class="my-dotted-border" src="/webapp/result/input1.jpg" />
+        </div>    
+        <div class="col-lg-12">
+            <h3>Result</h3>
+            <img class="my-dotted-border" src="/webapp/result/out1.jpg" />
+        </div>
+    </div>
+
+    </div>
+
+    <div class="row text-centered">
+
+        <div class="col-lg-12">
+            <a class="btn btn-info" href="/webapp/index.html"> GO BACK </a>
+        </div>
+    </div>
+    Some more text here!
+  </div>
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/plugins/canvas-to-blob.min.js"></script>
+    <script src="js/fileinput.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+ </body>
+</html>
